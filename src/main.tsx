ecf25ae4215d50e06384b3416b2eb38dfd26f02c
Razvan Radulescu <43811028+h3xxit@users.noreply.github.com>
2026-04-03 10:15:37 +0200

    Remove other version

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
+import CanvasApp from './CanvasApp';
+import './canvas.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CanvasApp />
   </StrictMode>,
 );
